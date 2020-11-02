f61e518552c4238890cc565b3154475d2207136f
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-11-02 12:52:48 -0500

    nerrs site metadata

--- data/get_nerrs_data.R
+++ data/get_nerrs_data.R
@@ -2,14 +2,15 @@ library(SWMPr)
 library(dplyr)
 library(readr)
 
+wells <- c("weldnwq", "welhtwq", "welinwq", "wellmwq", "welmlwq", "welsmwq", 
+           "welupwq")
+waquoit <- c("wqbcbwq", "wqbcrwq", "wqbctwq", "wqbmhwq", "wqbmpwq", "wqbnbwq", 
+             "wqbnswq", "wqbslwq")
+narr_bay <- c("narncwq", "narpcwq", "nartbwq", "nartswq", "nartwwq")
+grt_bay <- c("grbgbwq", "grblrwq", "grborwq", "grbsqwq")
+sites <- c(wells, waquoit, narr_bay, grt_bay)
+
 if(!file.exists("data/ne_nerrs_wq_2020.csv")){
-  wells <- c("weldnwq", "welhtwq", "welinwq", "wellmwq", "welmlwq", "welsmwq", 
-             "welupwq")
-  waquoit <- c("wqbcbwq", "wqbcrwq", "wqbctwq", "wqbmhwq", "wqbmpwq", "wqbnbwq", 
-               "wqbnswq", "wqbslwq")
-  narr_bay <- c("narncwq", "narpcwq", "nartbwq", "nartswq", "nartwwq")
-  grt_bay <- c("grbgbwq", "grblrwq", "grborwq", "grbsqwq")
-  sites <- c(wells, waquoit, narr_bay, grt_bay)
   ne_nerrs_wq <- data.frame()
   for(i in sites){ 
     err <- tryCatch(
@@ -28,4 +29,7 @@ if(!file.exists("data/ne_nerrs_wq_2020.csv")){
  ne_nerrs_wq <- read_csv("data/ne_nerrs_wq_2020.csv", guess_max = 300000) 
 }               
 
-
+ne_nerrs_sites <- site_codes()
+ne_nerrs_sites %>%
+  filter(station_code %in% sites) %>%
+  View()
